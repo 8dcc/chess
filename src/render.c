72db6e946a63791e7ee76a757d34968be2a2d121
8dcc <8dcc.git@gmail.com>
2025-11-25 17:01:00 +0100

    Add basic color support to 'render.c'

--- src/render.c
+++ src/render.c
@@ -28,7 +28,136 @@
 #define MARGIN_X 2 /* characters */
 #define MARGIN_Y 1 /* characters */
 
-static bool addfmt(const char* fmt, ...) {
+/*
+ * Enumeration with all possible render colors. These values will be used as IDs
+ * for the ncurses colors.
+ */
+enum ERenderColors {
+    RENDER_COL_INVALID = 0, /* Zero is not a valid index */
+
+    RENDER_COL_DEFAULT,
+    RENDER_COL_PIECE,
+    RENDER_COL_BORDER,
+
+    NUM_RENDER_COLORS, /* Must be last */
+};
+
+/*
+ * Structure representing a color configuration. The 'foreground' and
+ * 'background' members should be ncurses color indices, not RGB values.
+ */
+typedef struct {
+    bool is_bold;
+    bool is_dim;
+    int foreground;
+    int background;
+} ColorInfo;
+
+/*----------------------------------------------------------------------------*/
+
+/*
+ * Array with the color configurations for all color categories in the program.
+ */
+static const ColorInfo g_render_colors[] = {
+    [RENDER_COL_DEFAULT] = {
+      .is_bold    = false,
+      .is_dim     = false,
+      .foreground = COLOR_WHITE,
+      .background = COLOR_BLACK,
+    },
+    [RENDER_COL_PIECE] = {
+      .is_bold    = true,
+      .is_dim     = false,
+      .foreground = COLOR_WHITE,
+      .background = COLOR_BLACK,
+    },
+    [RENDER_COL_BORDER] = {
+      .is_bold    = false,
+      .is_dim     = true,
+      .foreground = COLOR_WHITE,
+      .background = COLOR_BLACK,
+    },
+};
+
+/*----------------------------------------------------------------------------*/
+
+/*
+ * Initialize the ncurses color configuration for the program. This function
+ * returns true on success, or false otherwise.
+ *
+ * Note that, if the terminal doesn't support colors, this function still
+ * returns successfully, and the program should simply not use them.
+ */
+static bool init_colors(void) {
+    /* The terminal doesn't support colors, we won't render them */
+    if (!has_colors())
+        return true;
+
+    /* Initialize ncurses color support */
+    if (start_color() == ERR || use_default_colors() == ERR)
+        return false;
+
+    /* Initialize each color pair, ensuring the colors are supported */
+    for (size_t i = RENDER_COL_DEFAULT; i < NUM_RENDER_COLORS; i++) {
+        if (g_render_colors[i].foreground >= COLORS ||
+            g_render_colors[i].background >= COLORS)
+            return false;
+
+        init_pair(i,
+                  g_render_colors[i].foreground,
+                  g_render_colors[i].background);
+    }
+
+    return true;
+}
+
+/*
+ * Change to the specified color ID using ncurses routines.
+ *
+ * Note that, if the terminal doesn't support colors, this function still
+ * returns successfully.
+ */
+static bool change_color(enum ERenderColors color) {
+    /* If the terminal doesn't support colors, ignore successfully */
+    if (!has_colors() || !can_change_color())
+        return true;
+
+    if (attron(COLOR_PAIR(color)) == ERR)
+        return false;
+    if (g_render_colors[color].is_bold && attron(A_BOLD) == ERR)
+        return false;
+    if (g_render_colors[color].is_dim && attron(A_DIM) == ERR)
+        return false;
+
+    return true;
+}
+
+/*
+ * Reset the terminal colors using ncurses routines, after 'old_color' was used.
+ *
+ * Note that, if the terminal doesn't support colors, this function still
+ * returns successfully.
+ */
+static bool reset_color(enum ERenderColors old_color) {
+    /* If the terminal doesn't support colors, ignore successfully */
+    if (!has_colors() || !can_change_color())
+        return true;
+
+    if (attroff(COLOR_PAIR(old_color)) == ERR)
+        return false;
+    if (g_render_colors[old_color].is_bold && attroff(A_BOLD) == ERR)
+        return false;
+    if (g_render_colors[old_color].is_dim && attroff(A_DIM) == ERR)
+        return false;
+
+    return true;
+}
+
+/*
+ * Print the specified formatted text into the screen with the specified color,
+ * using ncurses routines.
+ */
+static bool addfmt_colored(enum ERenderColors color, const char* fmt, ...) {
     va_list va;
 
     va_start(va, fmt);
@@ -46,24 +175,25 @@ static bool addfmt(const char* fmt, ...) {
     const int written = vsnprintf(buffer, data_size + 1, fmt, va);
     va_end(va);
 
-    if (written < 0) {
-        free(buffer);
-        return false;
-    }
+    bool result = true;
 
-    const int result = addstr(buffer);
+    result = result && (written >= 0);
+    result = result && change_color(color);
+    result = result && (addstr(buffer) != ERR);
+    result = result && reset_color(color);
+
     free(buffer);
-
-    return result != ERR;
+    return result;
 }
 
 /*----------------------------------------------------------------------------*/
 
 bool render_startup(void) {
-    return initscr() != NULL &&         /* Init ncurses */
-           raw() != ERR &&              /* Scan input without pressing enter */
-           noecho() != ERR &&           /* Don't print when typing */
-           keypad(stdscr, true) != ERR; /* Enable keypad (arrow keys) */
+    return initscr() != NULL && /* Init ncurses */
+           raw() != ERR &&      /* Scan input without pressing enter */
+           noecho() != ERR &&   /* Don't print when typing */
+           keypad(stdscr, true) != ERR && /* Enable keypad (arrow keys) */
+           init_colors();                 /* Initialize ncurses color pairs */
 }
 
 void render_cleanup(void) {
@@ -75,24 +205,26 @@ bool render_board(const Board* board) {
 
     /* Initial border */
     for (size_t x = 0; x < board->width; x++)
-        addstr("+---");
-    addch('+');
+        addfmt_colored(RENDER_COL_BORDER, "+---");
+    addfmt_colored(RENDER_COL_BORDER, "+");
 
     for (size_t y = 0; y < board->height; y++) {
         move(MARGIN_Y + 1 + (y * 2), MARGIN_X);
         /* Row pieces */
         for (size_t x = 0; x < board->width; x++)
-            if (!addfmt("| %c ",
-                        board_cell_get_char(
-                          &board->cells[board->width * y + x])))
+            if (!addfmt_colored(RENDER_COL_BORDER, "|") ||
+                !addfmt_colored(RENDER_COL_PIECE,
+                                " %c ",
+                                board_cell_get_char(
+                                  &board->cells[board->width * y + x])))
                 return false;
-        addch('|');
+        addfmt_colored(RENDER_COL_BORDER, "|");
 
         /* Border after each row */
         move(MARGIN_Y + 1 + (y * 2) + 1, MARGIN_X);
         for (size_t x = 0; x < board->width; x++)
-            addstr("+---");
-        addch('+');
+            addfmt_colored(RENDER_COL_BORDER, "+---");
+        addfmt_colored(RENDER_COL_BORDER, "+");
     }
 
     /* After rendering, move terminal cursor to the player cursor */
